0977145094ddb77ede32a49ba7d602a0c9e29b48
Hugo David Moreno Llamas <hmorenollamas@gmail.com>
2026-03-22 21:22:30 -0600

    Fix: Permitir todos los hosts en Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: true // Esto le dice a Vite que acepte cualquier dominio
+  },
+  preview: {
+    host: true,
+    port: 5173,
+    allowedHosts: true // Esto es para cuando corres la versión de producción
+  }
 })
