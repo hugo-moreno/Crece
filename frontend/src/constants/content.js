8cf6570e093b892998f54fb7a5cb5eb0c0565131
Hugo David Moreno Llamas <hmorenollamas@gmail.com>
2026-03-23 13:57:39 -0600

    creo que ya quedo todo con las diapositivas

--- frontend/src/constants/content.js
+++ frontend/src/constants/content.js
@@ -1,12 +1,12 @@
 export const cursoContenido = {
   "1": { 
     titulo: "Word para Principiantes",
-    instructor: "Leonel Martinez [cite: 217]",
+    instructor: "Leonel Martinez",
     modulos: Array.from({ length: 20 }, (_, i) => ({
       num: i + 1,
       tema: `Módulo ${i + 1}`,
-      // USAMOS / EN LUGAR DE \
-      img: `/assets/cursos/word/Diapositiva${i + 1}.png` 
+      // CAMBIAMOS .png por .PNG
+      img: `/assets/cursos/word/Diapositiva${i + 1}.PNG` 
     }))
   },
   "2": { 
@@ -15,16 +15,16 @@ export const cursoContenido = {
     modulos: Array.from({ length: 20 }, (_, i) => ({
       num: i + 1,
       tema: `Módulo ${i + 1}`,
-      img: `/assets/cursos/excel/Diapositiva${i + 1}.png`
+      img: `/assets/cursos/excel/Diapositiva${i + 1}.PNG`
     }))
   },
   "3": { 
     titulo: "PowerPoint para Principiantes",
-    instructor: "Susana Morales [cite: 5]",
+    instructor: "Susana Morales",
     modulos: Array.from({ length: 20 }, (_, i) => ({
       num: i + 1,
       tema: `Módulo ${i + 1}`,
-      img: `/assets/cursos/powerpoint/Diapositiva${i + 1}.png`
+      img: `/assets/cursos/powerpoint/Diapositiva${i + 1}.PNG`
     }))
   },
   "4": { 
@@ -33,7 +33,7 @@ export const cursoContenido = {
     modulos: Array.from({ length: 17 }, (_, i) => ({
       num: i + 1,
       tema: `Módulo ${i + 1}`,
-      img: `/assets/cursos/administracion/Diapositiva${i + 1}.png`
+      img: `/assets/cursos/administracion/Diapositiva${i + 1}.PNG`
     }))
   }
 };
